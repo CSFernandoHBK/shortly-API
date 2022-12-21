53513d4ea89673fd7be01bbeb191963bde104f3c
CSFernandoHBK <fernandotam2@hotmail.com>
2022-12-21 15:03:54 -0300

    route urls/shorten

--- src/controllers/urls.controller.js
+++ src/controllers/urls.controller.js
@@ -2,10 +2,41 @@ import { connectionDB } from "../database/db.js";
 import { nanoid } from 'nanoid';
 
 export async function linkGenerator(req, res){
-    console.log("entrou aqui");
+    const {authorization} = req.headers;
+    const completeUrl = req.body.url;
+
+    if(!authorization){
+        return res.sendStatus(401);
+    }
+
+    if(!completeUrl){
+        return res.sendStatus(400);
+    }
 
     try{
-        return res.send(req.body)
+        const token = authorization?.replace("Bearer ", "")
+        if(!token || token === "Bearer"){
+            return res.sendStatus(401);
+        }
+
+        const session = await connectionDB.query(`
+        SELECT * FROM sessions WHERE token = '${token}'
+        `);
+
+        if(!session.rows[0]){
+            return res.sendStatus(401);
+        };
+
+        const {userId} = session.rows[0];
+
+        const compactUrl = nanoid(8);
+
+        await connectionDB.query(`
+            INSERT INTO links ("userId", "completeLink", "compactLink", "clicks")
+            VALUES ('${userId}', '${completeUrl}', '${compactUrl}', '0')
+        `)
+
+        return res.send({"shortUrl": `${compactUrl}`});
     } catch(err){
         console.log(err);
         res.status(500).send(err.message);
